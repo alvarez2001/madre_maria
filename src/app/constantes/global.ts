7a882732d53c7965c8ac3042e535ad5210cb4884
alvarez2001 <josemiguelalvarez2014@gmail.com>
2020-07-25 23:23:29 -0400

    v-1 before universal

--- src/app/constantes/global.ts
+++ src/app/constantes/global.ts
@@ -1,4 +1,4 @@
 export const Global = {
-  url:'http://192.168.43.116/software/public/api/'
-/*   url:'https://ueiamadremaria.com/sistema/api/' */
+  /* url:'http://192.168.43.116/software/public/api/' */
+  url:'https://ueiamadremaria.com/sistema/api/'
 };
